588276bc8bfb9f13141426a0f2dca9f08f142a5d
Simon Bates <simon@bitstructures.com>
2016-03-10 14:20:37 -0500

    NOJIRA: Update licensing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
 /*
-Copyright 2015 OCAD University
+Copyright 2015, 2016 OCAD University
 
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
 
 You may obtain a copy of the License at
-https://raw.githubusercontent.com/GPII/nexus/master/LICENSE
+https://raw.githubusercontent.com/GPII/nexus/master/LICENSE.txt
 */
 
 /* global module */
--- nexus.js
+++ nexus.js
@@ -1,3 +1,13 @@
+/*
+Copyright 2015, 2016 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://raw.githubusercontent.com/GPII/nexus/master/LICENSE.txt
+*/
+
 "use strict";
 
 var kettle = require("kettle");
