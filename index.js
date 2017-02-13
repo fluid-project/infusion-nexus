7b175590a7f886652b87289ba42d0a8b57567dc4
Simon Bates <simon@bitstructures.com>
2017-02-13 13:21:54 -0500

    GPII-2177: Rename NexusConfig.js to CoOccurrenceEngine.js

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ var fluid = require("infusion");
 
 fluid.module.register("gpii-nexus", __dirname, require);
 require("./src/Nexus.js");
-require("./src/NexusConfig.js");
+require("./src/CoOccurrenceEngine.js");
